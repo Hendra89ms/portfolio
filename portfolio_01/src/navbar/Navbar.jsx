d207d71bd5b070dad6b01c20f2c0de85a2e67d58
Hendra manase sitorus <72810939+Hendra89ms@users.noreply.github.com>
2022-09-26 18:49:00 +0700

    menambah fitur responsive

--- portfolio_01/src/navbar/Navbar.jsx
+++ portfolio_01/src/navbar/Navbar.jsx
@@ -1,30 +1,57 @@
 import React from 'react'
+import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { FaTimes } from 'react-icons/fa';
+
 
 const Navbar = () => {
-    return (
-        <nav className='flex justify-between pl-10 items-center h-[70px] text-white font-bold'>
-            <h1 className='text-2xl select-none'>Hendra <span className='text-[#0891B2]'>Manase</span> Sitorus</h1>
 
-            <menu className='flex-none'>
-                <ul className='flex items-center justify-center gap-[40px] bg-white w-[500px] text-black h-[70px] rounded-bl-full'>
-                    <li>
-                        <a href="#home" className='hover:text-[#0891B2] duration-300'>Home</a>
+    const [nav, setNav] = useState(false)
+
+    const links = [
+        {
+            id: 1,
+            link: 'home'
+        },
+        {
+            id: 2,
+            link: 'about'
+        },
+        {
+            id: 3,
+            link: 'portfolio'
+        },
+        {
+            id: 4,
+            link: 'experience'
+        },
+        {
+            id: 5,
+            link: 'contact'
+        }
+    ]
+
+
+    return (
+        <nav className='flex justify-between items-center h-[70px] text-white font-bold'>
+            <h1 className='text-2xl pl-10 select-none'>Hendra <span className='text-[#0891B2]'>Manase</span> Sitorus</h1>
+
+
+            <ul className='hidden md:flex lg:flex items-center gap-7 pl-16 bg-white rounded-bl-full h-[70px] w-[500px]'>
+                {links.map(({ id, link }) => (
+                    <li
+                        key={id}
+                        className="flex gap-5 cursor-pointer capitalize font-medium text-black hover:scale-105 duration-200 hover:text-[#0891B2]"
+                    >
+                        {link}
                     </li>
-                    <li>
-                        <a href="#about" className='hover:text-[#0891B2] duration-300'>About</a>
-                    </li>
-                    <li>
-                        <a href="skils" className='hover:text-[#0891B2] duration-300'>Skills</a>
-                    </li>
-                    <li>
-                        <a href="#project" className='hover:text-[#0891B2] duration-300'>Project</a>
-                    </li>
-                    <li><a href="#Contact" className='hover:text-[#0891B2] duration-300'>
-                        Contact
-                    </a>
-                    </li>
-                </ul>
-            </menu>
+                ))}
+            </ul>
+            <div className='pr-4  cursor-pointer md:hidden duration-300' onClick={() => setNav(!nav)}>
+                {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+
+            </div>
+
         </nav>
     )
 }
